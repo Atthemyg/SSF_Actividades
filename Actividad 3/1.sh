202c65df455b587289b5799d8ca8a716e7803891
Atteneri Bravo Barroso <atteneribravobarroso@gmail.com>
2026-02-25 23:00:38 +0000

    Hago el apartado 1

--- Actividad 3/1.sh
+++ Actividad 3/1.sh
@@ -3,3 +3,10 @@
 #CONTROL DE ERRORES
 set -euo pipefail
 
+DIAS=$1
+HORAS=$2
+SEGUNDOS=$3
+
+TOTAL_SEGUNDOS=$(( DIAS * 86400 + HORAS * 3600 + SEGUNDOS ))
+
+echo "Total de segundos: $TOTAL_SEGUNDOS"
